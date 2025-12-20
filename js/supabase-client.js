b526afe9e1e43c774aba09e150cf1b9ab034656e
unezac <selmaniabde78@gmail.com>
2025-12-20 14:33:09 +0100

    Setup Supabase connection

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -2,7 +2,7 @@
 // REPLACE THESE WITH YOUR OWN SUPABASE PROJECT DETAILS
 // You can find them in Supabase Dashboard -> Project Settings -> API
 const SUPABASE_URL = 'https://ecljdstmedvnfsxbiwkf.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_UG7DuQihHJbIbOL39rsDHQ_uMXaPYF0';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjbGpkc3RtZWR2bmZzeGJpd2tmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyMTU3MDUsImV4cCI6MjA4MTc5MTcwNX0.Xj_rSUxKg3lCPrZouAhOe0vPuHduPXza7-EGiMT_ujM';
 
 // Initialize Supabase Client
 // This assumes the Supabase JS library is loaded via CDN in the HTML file
